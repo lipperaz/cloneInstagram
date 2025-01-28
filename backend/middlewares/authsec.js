5cf63ab60673825c38dc6793bfed4c7f4f9d69f9
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-01-28 15:05:47 -0300

    Update authsec.js

--- backend/middlewares/authsec.js
+++ backend/middlewares/authsec.js
@@ -7,16 +7,16 @@ const authSec = async (req, res, next) => {
     const token = authHeader && authHeader.split(" ")[1];
 
     // Check Header have Token
-    if(!token) return res.status(401).json({errors: ["Acesso negado!"]})
+    if(!token) return res.status(401).json({errors: ["Acesso negado!"]});
 
     // Check Token valid
     try {
-        const verified = jwt.verify(token, jwtSecret)
-        req.user = await User.findById(verified.id).select("-password")
+        const verified = jwt.verify(token, jwtSecret);
+        req.user = await User.findById(verified.id).select("-password");
         next();
 
-    } catch (errors) {
-        res.status(401).json({errors: ["Token invalido!"]})
+    } catch (error) {
+        res.status(401).json({errors: ["Token invalido!"]});
     }
 };
 
